232d8b4de3a478a51e2a4149ca0241546548b880
hzzjb <hzzjb@tairanchina.com>
2016-11-18 11:09:59 +0800

    update

---
+++ core-yc256-smart-api/src/main/java/com/yc256/smart/service/SmartMemberService.java
@@ -0,0 +1,11 @@
+package com.yc256.smart.service;
+
+/**
+ * SmartMemberService
+ *
+ * @author hzzjb
+ * @date 2016/11/18
+ */
+public interface SmartMemberService {
+
+}
---
+++ core-yc256-smart-dao/src/main/java/com/yc256/smart/service/SmartMemberServiceImpl.java
@@ -0,0 +1,11 @@
+package com.yc256.smart.service;
+
+/**
+ * SmartMemberServiceImpl
+ *
+ * @author hzzjb
+ * @date 2016/11/18
+ */
+public class SmartMemberServiceImpl implements SmartMemberService {
+
+}
